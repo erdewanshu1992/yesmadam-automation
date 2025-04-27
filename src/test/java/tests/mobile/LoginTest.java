8154f398534e356bd9974028832877fa5f36b8a9
Qadev <qadev423@gmail.com>
2025-04-28 01:00:24 +0530

    Change in test

--- src/test/java/tests/mobile/LoginTest.java
+++ src/test/java/tests/mobile/LoginTest.java
@@ -6,7 +6,6 @@ import com.opencsv.CSVReader;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import screen.mobile.LoginScreen;
-
 import java.io.FileReader;
 import java.util.List;
 
