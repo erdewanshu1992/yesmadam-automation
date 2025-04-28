c6d6d63cdc42140b918a608978108864837af910
Qadev <qadev423@gmail.com>
2025-04-28 11:53:02 +0530

    Change in test

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
-
 import java.net.MalformedURLException;
 
-public class BaseTest {
 
+public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
@@ -42,61 +41,3 @@ public class BaseTest {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package base;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
-//import utils.ConfigReader;
-//
-//import java.net.MalformedURLException;
-//
-//public class BaseTest {
-//
-//    protected WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setup() throws MalformedURLException {
-//        String platform = ConfigReader.getProperty("platform");  // 'web' or 'mobile'
-//        String browser = ConfigReader.getProperty("browser");
-//
-//        if (platform.equalsIgnoreCase("web")) {
-//            driver = DriverFactory.initWebDriver(browser);
-//            String url = ConfigReader.getProperty("url");
-//            if (url != null && !url.isEmpty()) {
-//                driver.get(url);
-//            } else {
-//                throw new RuntimeException("URL missing in config.properties");
-//            }
-//        } else if (platform.equalsIgnoreCase("mobile")) {
-//            driver = DriverFactory.initAndroidDriver();
-//            System.out.println("\u001B[33m Customer App is open now =>>: Good luck Dewanshu....👍😍🧚‍♀️ \u001B[0m");
-//        } else {
-//            throw new RuntimeException("Unsupported platform: " + platform);
-//        }
-//
-//        System.out.println("Tests are starting!");
-//    }
-//
-//    @AfterMethod
-//    public void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//            System.out.println("Tests are ending!");
-//        }
-//    }
-//}
